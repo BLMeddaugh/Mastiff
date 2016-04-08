334c7a52e9aeea101fd648d7c3c1bd3c56186d37
Suende <puma_tp@hotmail.com>
2016-04-08 16:30:48 -0500

    First Alpha Version for SenSem

---
+++ AlphaOne.Tests/Controllers/CommoditiesControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using NUnit.Framework;
+using Moq;
+using AlphaOne;
+using AlphaOne.Models;
+using HtmlAgilityPack;
+
+
+namespace AlphaOne.Tests.Controllers
+{
+    [TestFixture]
+    class CommoditiesControllerTest
+    {
+        [TestCase]
+        public void testCommodityPriceSetting()
+        {
+            HtmlWeb webget = new HtmlWeb();
+            HtmlDocument document = webget.Load("http://finance.yahoo.com/q?s=clk16.nym");
+            string price = document.DocumentNode.SelectSingleNode("id('yfs_l10_clk16.nym')").InnerHtml;
+            string type = "Crude Oil";
+
+            Commodity commodity = new Commodity();
+            commodity.Price = float.Parse(price);
+            Assert.IsNotNull(commodity.Price);
+        }
+       
+        
+        
+
+    }
+}
--- AlphaOne/Models/Commodity.cs
+++ AlphaOne/Models/Commodity.cs
@@ -8,6 +8,14 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace AlphaOne.Models
 {
+    public interface ICommodity
+    {
+        IList<Commodity> FindAll();
+         int ID { get; set; }
+         float Price { get; set; }
+         DateTime Date { get; set; }
+         string Type { get; set; }
+    }
     public class Commodity
     {
         public int ID { get; set; }
@@ -16,4 +24,5 @@ namespace AlphaOne.Models
         public string Type { get; set; }
 
     }
+
 }
