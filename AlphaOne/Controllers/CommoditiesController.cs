d34f7c58c8654b3e983d8de98c070030f865f1b2
Suende <puma_tp@hotmail.com>
2016-04-08 14:03:23 -0500

    Auto Refresh included, view for Commodities improved

--- AlphaOne/Controllers/CommoditiesController.cs
+++ AlphaOne/Controllers/CommoditiesController.cs
@@ -17,10 +17,12 @@ namespace AlphaOne.Models
         // GET: Commodities
         public ActionResult Index()
         {
+
             //use HAP to get oil price
             HtmlWeb webget = new HtmlWeb();
             HtmlDocument document = webget.Load("http://finance.yahoo.com/q?s=clk16.nym");
             string price = document.DocumentNode.SelectSingleNode("id('yfs_l10_clk16.nym')").InnerHtml;
+            string type = "Crude Oil";
 
             Commodity commodity = new Commodity();
             commodity.Date = DateTime.Now;
