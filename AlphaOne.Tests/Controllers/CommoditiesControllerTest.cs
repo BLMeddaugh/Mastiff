e533e6fbd8aed7077101e6f19bc4f115cd0363da
Suende <puma_tp@hotmail.com>
2016-04-29 12:17:50 -0500

    New Unit Tests

--- AlphaOne.Tests/Controllers/CommoditiesControllerTest.cs
+++ AlphaOne.Tests/Controllers/CommoditiesControllerTest.cs
@@ -8,17 +8,29 @@ namespace AlphaOne.Tests.Controllers
     [TestClass]
     public class CommoditiesControllerTest
     {
+        Commodity commodity = new Commodity();
         [TestMethod]
-        public void TestMethod1()
+        public void GrabbingAndApplyingPrices()
         {
             HtmlWeb webget = new HtmlWeb();
             HtmlDocument document = webget.Load("http://finance.yahoo.com/q?s=clk16.nym");
-            string price = document.DocumentNode.SelectSingleNode("id('yfs_l10_clk16.nym')").InnerHtml;
-            string type = "Crude Oil";
-
-            Commodity commodity = new Commodity();
+            string price = document.DocumentNode.SelectSingleNode("id('yfs_l10_clk16.nym')").InnerHtml; 
             commodity.Price = float.Parse(price);
             Assert.IsNotNull(commodity.Price);
+        }
+        [TestMethod]
+        public void CorrectType()
+        {
+            string type = "Crude Oil";
+            commodity.Type = type;
+            Assert.Equals(commodity.Type, "Crude Oil");
+        }
+        [TestMethod]
+        public void DateBeingSet()
+        {
+           var curDate = DateTime.Now;
+            commodity.Date = curDate;
+            Assert.AreEqual(commodity.Date, curDate);
         }
     }
 }
