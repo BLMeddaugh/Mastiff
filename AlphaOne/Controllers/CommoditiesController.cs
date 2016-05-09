4ad423dcd57fea664eebbd41fd875e3465593558
Suende <puma_tp@hotmail.com>
2016-05-09 13:53:43 -0500

    Added currencies and some CSS need to get charts working

--- AlphaOne/Controllers/CommoditiesController.cs
+++ AlphaOne/Controllers/CommoditiesController.cs
@@ -29,48 +29,12 @@ namespace AlphaOne.Models
         // GET: Commodities
         public ActionResult Index()
         {
+            makeCommoditity("http://finance.yahoo.com/q?s=clk16.nym", "id('yfs_l10_clk16.nym')", "Oil");
+            makeCommoditity("http://finance.yahoo.com/futures?t=grains", "id('yfs_l10_ck16.cbt')", "Corn");
 
-            //use HAP to get oil price
-            HtmlWeb webget = new HtmlWeb();
-            HtmlDocument document = webget.Load("http://finance.yahoo.com/q?s=clk16.nym");
-            string price = document.DocumentNode.SelectSingleNode("id('yfs_l10_clk16.nym')").InnerHtml;
-            //make oil object
-            Commodity oil = new Commodity();
-            oil.Date = DateTime.Now;
-            oil.Price = float.Parse(price);
-            oil.Type = "Oil";
-            repository.Insert(oil);
-            
-            //db.Commodities.Add(oil);
-            //db.SaveChanges();
-            System.Threading.Thread.Sleep(100);
+            makeCommoditity("http://finance.yahoo.com/q?s=USDRUB=X", "id('yfs_l10_usdrub=x')", "RUP");
+            makeCommoditity("http://finance.yahoo.com/q?s=USDBRL=X", "id('yfs_l10_usdbrl=x')", "BRL");
 
-            //HAP to get soy
-            webget = new HtmlWeb();
-            document = webget.Load("http://finance.yahoo.com/futures?t=grains");
-            price = document.DocumentNode.SelectSingleNode("id('yfs_l10_ck16.cbt')").InnerHtml;
-            //make soy object
-            Commodity corn = new Commodity();
-            corn.Price = float.Parse(price);
-            corn.Type = "Corn";
-            corn.Date = DateTime.Now;
-            repository.Insert(corn);
-            //db.Commodities.Add(corn);
-            //db.SaveChanges();
-
-            //HAP to get rupee
-            webget = new HtmlWeb();
-            document = webget.Load("http://finance.yahoo.com/q?s=USDRUB=X");
-            price = document.DocumentNode.SelectSingleNode("id('yfs_l10_usdrub=x')").InnerHtml;
-            //make soy object
-            Commodity rupee = new Commodity();
-            rupee.Price = float.Parse(price);
-            rupee.Type = "Rupee";
-            rupee.Date = DateTime.Now;
-            repository.Insert(rupee);
-            repository.Save();
-            //db.Commodities.Add(rupee);
-            //db.SaveChanges();
 
             //int latest = db.Commodities.LastOrDefault().ID;
             //int i = 1;
@@ -86,112 +50,49 @@ namespace AlphaOne.Models
             return View(db.Commodities.ToList());
         }
 
-        
+        public void makeCommoditity(string site, string x, string type)
+        {
+
+            HtmlWeb webget = new HtmlWeb();
+            HtmlDocument document = webget.Load(site);
+            string price = document.DocumentNode.SelectSingleNode(x).InnerHtml;
+            Commodity c = new Commodity();
+            c.Date = DateTime.Now;
+            c.Price = float.Parse(price);
+            c.Type = type;
+            repository.Insert(c);
+            repository.Save();
+        }
 
         // GET: Commodities/Details/5
         public ActionResult Details(int? id)
         {
-            
+
             var com = db.Commodities.Where(x => x.Type == "Oil").ToList();
             ArrayList prices = new ArrayList();
             ArrayList dates = new ArrayList();
-            foreach(var Commodity in com)
+            foreach (var Commodity in com)
             {
                 prices.Add(Commodity.Price);
                 dates.Add(Commodity.Date);
             }
             ViewBag.OilPrices = prices;
             ViewBag.OilDates = dates;
+
             return View();
         }
 
-        // GET: Commodities/Create
-        public ActionResult Create()
+        public ActionResult OilGraph()
         {
-            return View();
-        }
-
-        // POST: Commodities/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Price,Type,Date")] Commodity commodity)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Commodities.Add(commodity);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(commodity);
-        }
-
-        // GET: Commodities/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Commodity commodity = db.Commodities.Find(id);
-            if (commodity == null)
-            {
-                return HttpNotFound();
-            }
-            return View(commodity);
-        }
-
-        // POST: Commodities/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Price,Type,Date")] Commodity commodity)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(commodity).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(commodity);
-        }
-
-        // GET: Commodities/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Commodity commodity = db.Commodities.Find(id);
-            if (commodity == null)
-            {
-                return HttpNotFound();
-            }
-            return View(commodity);
-        }
-
-        // POST: Commodities/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Commodity commodity = db.Commodities.Find(id);
-            db.Commodities.Remove(commodity);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            var myChart = new Chart(width: 600, height: 400)
+           .AddTitle("Price History")
+           .AddSeries(
+              chartType: "line",
+               name: "Oil Price",
+               xValue: ViewBag.OilDates,
+               yValues: ViewBag.OilPrices)
+           .GetBytes("png");
+           return File(myChart, "image/bytes");
         }
     }
 }
